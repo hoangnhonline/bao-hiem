da7b5a488769acdead3150bc3f1809e06fa7ef22
HoangNH <hoangnh@evolableasia.vn>
2016-01-18 15:44:27 +0700

    abc

--- blocks/step1.php
+++ blocks/step1.php
@@ -43,65 +43,17 @@
                 <input class="form-control" name="persons" id="persons" />
               </div>
           </div><!--col-md-6-->
-          <div class="col-md-12" id="hinh-thuc">
-            <label class="title">Hình thức du lịch:</label>
-            <label class="radio-inline">
-              <input type="radio" name="type" value="Individual" checked>Cá nhân
-            </label>
-            <label class="radio-inline">
-              <input type="radio" name="type" value="Family">Gia đình
-            </label>                                
-          </div>
-          <div class="col-md-12" id="co-ban">
-            <p class="title">Quyền lợi bảo hiểm cơ bản bắt buộc: <span>(Xem chi tiết)</span></p>
-            <div class="col-md-4">
-            <label class="title">1. Chi phí y tế:</label>
+          <div class="col-md-6" >
+            <div id="hinh-thuc">
+              <label class="title">Hình thức du lịch:</label>
+              <label class="radio-inline">
+                <input type="radio" name="type" value="Individual" checked>Cá nhân
+              </label>
+              <label class="radio-inline">
+                <input type="radio" name="type" value="Family">Gia đình
+              </label>          
             </div>
-            <div class="col-md-8">
-              <div class="col-md-4">
-              <label class="radio-inline">
-                <input type="radio" name="medical_plan" value="A">Hạng A
-              </label>
-              </div>
-              <div class="col-md-4">
-              <label class="radio-inline">
-                <input type="radio" name="medical_plan" value="B" checked>Hạng B
-              </label>
-              </div>
-              <div class="col-md-4">
-              <label class="radio-inline">
-                <input type="radio" name="medical_plan" value="C">Hạng C
-              </label>
-              </div>
-            </div>
-            </div><!--col-md-12-->
-          <div class="col-md-12" id="ca-nhan">
-            <div class="col-md-4"><label class="title">2. Tai nạn cá nhân:</label></div>
-            <div class="col-md-8">
-              <div class="col-md-6">
-              <label class="radio-inline">
-                <input type="radio" name="personal_accident" value="1">400 triệu VND
-              </label>
-              </div>
-              <div class="col-md-6">
-              <label class="radio-inline">
-                <input type="radio" name="personal_accident" value="2" checked>1 tỷ VND
-              </label>
-              </div>
-              <div class="col-md-6">                                  
-              <label class="radio-inline">
-                <input type="radio" name="personal_accident" value="3">2 tỷ VND
-              </label>
-              </div>
-              <div class="col-md-6">
-              <label class="radio-inline">
-                <input type="radio" name="personal_accident" value="4">5 tỷ VND
-              </label>
-              </div>
-            </div>
-          </div><!--col-md-12-->
-
-          <div class="col-md-12" id="lua-chon">
+            <div id="lua-chon">
             <p class="title">Quyền lợi bảo hiểm lựa chọn:<span>(Xem chi tiết)</span></p>
             <div class="col-md-6">
             <label class="title">-  Bảo hiểm sự cố bất ngờ:</label>
@@ -119,25 +71,80 @@
               </div>                                 
             </div>
             </div><!--col-md-12-->
-            <div class="col-md-12" style="text-align:right">
-              <button class="btn-primary btn" id="btnXemphi">Xem phí</button>
-            </div>
+            
             
           </div>
+          <div class="col-md-6">                      
+            <div class="col-md-12" id="co-ban">
+              <p class="title">Quyền lợi bảo hiểm cơ bản bắt buộc: <span>(Xem chi tiết)</span></p>
+              <div class="col-md-4">
+              <label class="title">1. Chi phí y tế:</label>
+              </div>
+              <div class="col-md-8">
+                <div class="col-md-4">
+                <label class="radio-inline">
+                  <input type="radio" name="medical_plan" value="A">Hạng A
+                </label>
+                </div>
+                <div class="col-md-4">
+                <label class="radio-inline">
+                  <input type="radio" name="medical_plan" value="B" checked>Hạng B
+                </label>
+                </div>
+                <div class="col-md-4">
+                <label class="radio-inline">
+                  <input type="radio" name="medical_plan" value="C">Hạng C
+                </label>
+                </div>
+              </div>
+              </div><!--col-md-12-->
+            <div class="col-md-12" id="ca-nhan">
+              <div class="col-md-4"><label class="title">2. Tai nạn cá nhân:</label></div>
+              <div class="col-md-8">
+                <div class="col-md-6">
+                <label class="radio-inline">
+                  <input type="radio" name="personal_accident" value="1">400 triệu VND
+                </label>
+                </div>
+                <div class="col-md-6">
+                <label class="radio-inline">
+                  <input type="radio" name="personal_accident" value="2" checked>1 tỷ VND
+                </label>
+                </div>
+                <div class="col-md-6">                                  
+                <label class="radio-inline">
+                  <input type="radio" name="personal_accident" value="3">2 tỷ VND
+                </label>
+                </div>
+                <div class="col-md-6">
+                <label class="radio-inline">
+                  <input type="radio" name="personal_accident" value="4">5 tỷ VND
+                </label>
+                </div>
+              </div>
+            </div><!--col-md-12-->
+          </div>
+          
+            <div class="col-sm-10">
+              <h3 class="panel-title">Cách thức mua bảo hiểm</h3>
+              <ul>
+              <li class="col-sm-6">1. Trực tiếp lên website nhanh nhất - tiện nhất</li>
+              <li class="col-sm-6">2. Gọi vào Hotline:0983.787.787 - 0987.520.520</li>
+              <li class="col-sm-6">3. Qua chat:</li>
+              <li class="col-sm-6">4. Đến trực tiếp đại lý Én Việt trên toàn quốc</li>
+            </ul>
+            </div>
+            <div class="col-sm-2">
+              <button class="btn-primary btn btn-lg" id="btnXemphi">Xem phí</button>
+            </div>
+          
+          </div>
+          
+
+          
       </div>
       <div class="clearfix"></div>
-      <div class="panel panel-default">
-        <div class="panel-heading">
-          <h3 class="panel-title">Cách thức mua bảo hiểm</h3>
-        </div>
-        <div class="panel-body">
-          <ul>
-            <li>1. Trực tiếp lên website nhanh nhất - tiện nhất</li>
-            <li>2. Gọi vào Hotline:0983.787.787 - 0987.520.520</li>
-            <li>3. Qua chat:</li>
-            <li>4. Đến trực tiếp đại lý Én Việt trên toàn quốc</li>
-          </ul>
-        </div>
-      </div>
       
-    </div><!--col-md-6-->
+      
+    </div><!--col-md-12-->
+    
--- step2.php
+++ step2.php
@@ -8,100 +8,58 @@
     <!-- Bootstrap -->
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="css/bootstrap.css">
-    <style type="text/css">
-    .tab-pane{padding: 5px}
-    .form-group{
-      margin-bottom: 5px !important;
-    }
-    label{
-      margin-bottom: 2px !important;  
-      font-weight: normal !important;      
-    }
-    #hinh-thuc{
-      margin-top: 15px;
-      margin-bottom: 15px;
-    }
-    #co-ban, #ca-nhan, #lua-chon{
-      margin-bottom: 15px;
-    }
-    .title {
-      
-      font-weight: bold !important
-    }
-    </style>
-      <style type="text/css">
-  #crumbs ul {
-    list-style: none;
-    display: inline-table;
-  }
-    #crumbs ul li {
-      display: inline;
-      padding: 2px;
-    }
-  
-    #crumbs ul li a {
-      display: block;
-      float: left;
-      text-align: center;
-      height: 40px;
-      background: #3498db;
-      position: relative;
-      margin: 0 10px 0 0; 
-      padding-top: 9px;      
-      font-size: 17px;
-      text-decoration: none;
-      color: #fff;
-          width: 100%;
-    }
-      #crumbs ul li a:after {
-        content: "";  
-        border-top: 20px solid transparent;
-        border-bottom: 20px solid transparent;
-          border-left: 20px solid #3498db;
-        position: absolute; right: -20px; top: 0;
-        z-index: 1;
-      }
-      
-      #crumbs ul li a:before {
-        content: "";  
-        border-top: 20px solid transparent;
-          border-bottom: 20px solid transparent;
-          border-left: 20px solid #FFF;
-        position: absolute; left: 0; top: 0;
-      }
-  
-        #crumbs ul li:first-child a {
-          border-top-left-radius: 10px; border-bottom-left-radius: 10px;    
-          padding-left: 15px;      
-        }
-        #crumbs ul li:first-child a:before {
-          display: none; 
-        }
-        
-        #crumbs ul li:last-child a {          
-          border-top-right-radius: 10px; border-bottom-right-radius: 10px;
-        }
-        #crumbs ul li:last-child a:after {
-          display: none; 
-        }
-      
-      #crumbs ul li a:hover, #crumbs ul li a.active {
-        background: #ff6400;
-      }
-        #crumbs ul li a:hover:after, #crumbs ul li a.active:after {
-          border-left-color: #ff6400;
-        }
-  </style>
+    <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
+    <header id="header" class="fixed-width"><section id="menu-base">
+      <div class="main-top-menu">
+      <div class="container">
+      <div class="row">
+      <div class="col-md-12">
+      <ul class="nav navbar-nav">
+        <li class="dropdown"><a class="dropdown-toggle icon-menu" href="#" data-toggle="dropdown">Menu</a>
+      <ul class="dropdown-menu main-nav">
+        <li><a href="/tra-ve-may-bay">Vé máy bay trực tuyến</a></li>
+        <li><a href="/dang-ki-visa-passport">Dịch vụ Visa - passport</a></li>
+        <li><a href="/dich-vu-dat-phong-khach-san/">Đặt phòng khách sạn</a></li>
+        <li><a href="http://www.worldtrans.vn/globalink/dich-vu-du-lich" target="_blank">Dịch vụ du lịch</a></li>
+        <li><a href="/dich-vu-bao-hiem-du-lich/">Bảo hiểm du lịch</a></li>
+      </ul>
+      </li>
+      </ul>
+      <div class="navbar-header"><a class="navbar-brand globalink-ticket" href="/">Globalink</a></div>
+      <ul class="nav navbar-nav navbar-right">
+        <li>Quý khách vui lòng liên hệ với chúng tôi: (+84) 08 399 00 567 - 08 399 00 365</li>
+        <li><a class="gift" href="/">Quà tặng</a></li>
+        <li><a class="info-member" href="/">Thông tin thành viên</a></li>
+        <li><a class="login" href="/">Đăng nhập</a></li>
+      </ul>
+      </div>
+      </div>
+      <!-- /.navbar-collapse -->
+
+      </div>
+      <!-- /.container-fluid -->
+
+      </div>
+      </section></header>
     <div class="container" style="margin-top:20px;">
+        <div class="col-sm-12">
+          <img src="img/bluecross.jpg" width="120" style="float:right">
+
+        </div>
+        <div class="col-sm-12 title">
+          <h3 class="tt-module"><span style="color: #14697e; font-size: 14pt;"><strong>BẢO HIỂM DU LỊCH</strong></span></h3>
+        </div>
+
         <div class="row">
-          
+
+            <form>
               <div>
                 <div id="crumbs">
                   <ul class="col-sm-12">
-                    <li class="col-sm-3"><a href="index.php">Chuyến đi và tính phí</a></li>
-                    <li class="col-sm-3"><a href="step2.php"  class="active">Thông tin người bảo hiểm</a></li>
+                    <li class="col-sm-3"><a href="index.php" >Chuyến đi và tính phí</a></li>
+                    <li class="col-sm-3"><a href="step2.php" class="active">Thông tin người bảo hiểm</a></li>
                     <li class="col-sm-3"><a href="step3.php">Đăng nhập hệ thống</a></li>
                     <li class="col-sm-3"><a href="step4.php">Xác nhận và thanh toán</a></li>
                     
@@ -111,12 +69,175 @@
                 <!-- Tab panes -->
                 <div class="tab-content">
                   <div role="tabpanel" class="tab-pane active" id="step1">
-                      <?php include "blocks/step2.php"; ?>                      
+                      <?php include "blocks/step2.php"; ?>                     
+                      
                   </div>                  
                 </div>
 
               </div>
-        
+            </form>
+            <div class="col-sm-12">
+      <h3 class="tt-module">Các tùy chọn khác</h3>
+    </div>
+    <div class="other-options" >
+      <div class="col-sm-3 ticket-option"><a href="/tra-ve-may-bay/"><img src="img/ve-may-bay-truc-tuyen1.jpg" alt="" class="img-responsive"></a>
+      <h2><a href="/tra-ve-may-bay/">Dịch vụ vé máy bay</a></h2>
+      </div>
+      <div class="col-sm-3 hotel-option"><a href="/dich-vu-dat-phong-khach-san/"><img src="img/dat-phong-khach-san1.jpg" alt="" class="img-responsive"></a>
+      <h2><a href="/dich-vu-dat-phong-khach-san/">Đặt phòng khách sạn</a></h2>
+      </div>
+      <div class="col-sm-3 visa-option"><a href="/dang-ki-visa-passport/"><img src="img/dich-vu-passport1.jpg" alt="" class="img-responsive"></a>
+      <h2><a href="/dang-ki-visa-passport/">Dịch vụ Visa - passport</a></h2>
+      </div>
+      <div class="col-sm-3 tour-option"><a href="http://www.worldtrans.vn/globalink/dich-vu-du-lich" target="_blank">
+        <img src="img/dich-vu-du-lich1.jpg" alt="" class="img-responsive"></a>
+      <h2><a href="http://www.worldtrans.vn/globalink/dich-vu-du-lich" target="_blank">Dịch vụ du lịch</a></h2>
+      </div>
+      <div class="col-sm-3 hidden"><a href="/dich-vu-bao-hiem-du-lich/"><img src="img/bao-hiem.jpg" alt="" class="img-responsive"></a>
+      <h2><a href="/dich-vu-bao-hiem-du-lich/">Bảo hiểm du lịch</a></h2>
+      </div>
+    </div>
+    <div class="col-sm-12" style="margin-top:20px">
+    <h3 class="tt-module"><span style="font-family: 'times new roman', times, serif; font-size: 14pt;">BẢO HIỂM BLUECROSS - BẢO VỆ QUYỀN LỢI CỦA BẠN</span></h3>
+    </div>
+    <div class="cheap-tickets_box col-sm-12">
+    
+    <ul class="row">
+      <li class="col-md-3"><span style="font-family: 'times new roman', times, serif; font-size: 14pt; color: #3366ff;"><strong>TRAVEL FLEX</strong></span></li>
+      <li class="col-md-3"><span style="font-family: 'times new roman', times, serif; font-size: 14pt; color: #3366ff;"><strong>TRAVEL FLEX - PREMIUM</strong></span></li>
+      <li class="col-md-3"><span style="font-family: 'times new roman', times, serif; font-size: 14pt; color: #3366ff;"><strong>QUYỀN LỢI CƠ BẢN&nbsp;</strong></span></li>
+      <li class="col-md-3"><span style="font-family: 'times new roman', times, serif; font-size: 14pt; color: #3366ff;"><strong>QUYỀN LỢI TỰ CHỌN</strong></span></li>
+    </ul>
+    </div>
+    
+    <div class="col-sm-12">
+    <div class="sign-up-newsletter_box col-sm-12">
+
+      <div class="col-md-7">
+      <div class="sign-up-tt" style="text-align: justify; width: 300px; float: left;"><span class="txt-dang-ky-mail" style="font-size: 16px;">Đăng ký nhận bản tin khuyến mãi</span>
+      <span style="font-size: 11px;">Nhận các ưu đãi tuyệt vời trong hộp thư đến của bạn!</span></div>
+      <div style="width: 300px; float: right;">
+
+      <!--Email subcriber-->
+      <div class="aign-up-mail"><script src="/wp-content/plugins/email-subscribers/widget/es-widget-page.js" type="text/javascript"></script>
+
+      <div class="es_msg"></div>
+
+      <input id="es_txt_name_pg" class="es_textbox_class" style="display: none;" maxlength="225" name="es_txt_name_pg" type="text" value=""> <input id="es_txt_email_pg" class="es_textbox_class" maxlength="225" name="es_txt_email_pg" type="text" value=""> <input id="es_txt_button_pg" class="es_textbox_button" name="es_txt_button_pg" type="button" value="Đăng ký"> <input id="es_txt_group_pg" name="es_txt_group_pg" type="hidden" value="Public">
+      </div>
+
+      <script>// <![CDATA[
+      function doSubscriptionEmail(){ es_submit_pages('http://globalink.vn'); }
+      // ]]&gt;</script>
+      <!--End Email subcriber-->
+
+      </div>
+      </div>
+      <div class="col-md-5 social-network" style="padding-right: 0;">
+
+      <span class="txt-cong-dong" style="font-size: 14px;">Cộng đồng</span>
+      <ul>
+        <li style="padding-top: 15px;">
+      <div id="fb-root" class=" fb_reset"><div style="position: absolute; top: -10000px; height: 0px; width: 0px;"><div><iframe name="fb_xdm_frame_http" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" title="Facebook Cross Domain Communication Frame" aria-hidden="true" tabindex="-1" id="fb_xdm_frame_http" src="http://staticxx.facebook.com/connect/xd_arbiter.php?version=42#channel=f2c14cdfb&amp;origin=http%3A%2F%2Fglobalink.vn" style="border: none;"></iframe><iframe name="fb_xdm_frame_https" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" title="Facebook Cross Domain Communication Frame" aria-hidden="true" tabindex="-1" id="fb_xdm_frame_https" src="https://staticxx.facebook.com/connect/xd_arbiter.php?version=42#channel=f2c14cdfb&amp;origin=http%3A%2F%2Fglobalink.vn" style="border: none;"></iframe></div></div><div style="position: absolute; top: -10000px; height: 0px; width: 0px;"><div></div></div></div>
+      <script>// <![CDATA[
+      (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5";
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));
+      // ]]&gt;</script>
+      <div class="fb-like fb_iframe_widget" style="overflow: hidden;" data-href="https://www.facebook.com/Globalink-Travel-1514350022209151/" data-layout="button_count" data-action="like" data-share="true" fb-xfbml-state="rendered" fb-iframe-plugin-query="action=like&amp;app_id=&amp;container_width=0&amp;href=https%3A%2F%2Fwww.facebook.com%2FGlobalink-Travel-1514350022209151%2F&amp;layout=button_count&amp;locale=en_US&amp;sdk=joey&amp;share=true"><span style="vertical-align: bottom; width: 130px; height: 20px;"><iframe name="f18f817104" width="1000px" height="1000px" frameborder="0" allowtransparency="true" allowfullscreen="true" scrolling="no" title="fb:like Facebook Social Plugin" src="http://www.facebook.com/v2.5/plugins/like.php?action=like&amp;app_id=&amp;channel=http%3A%2F%2Fstaticxx.facebook.com%2Fconnect%2Fxd_arbiter.php%3Fversion%3D42%23cb%3Df2161c4dac%26domain%3Dglobalink.vn%26origin%3Dhttp%253A%252F%252Fglobalink.vn%252Ff2c14cdfb%26relation%3Dparent.parent&amp;container_width=0&amp;href=https%3A%2F%2Fwww.facebook.com%2FGlobalink-Travel-1514350022209151%2F&amp;layout=button_count&amp;locale=en_US&amp;sdk=joey&amp;share=true" style="border: none; visibility: visible; width: 130px; height: 20px;" class=""></iframe></span></div></li>
+        <li><a class="icon-fb" title="" href="https://www.facebook.com/Globalink-Travel-1514350022209151/" target="_blank">Facebook</a></li>
+        <li><a class="icon-tt" title="">Twitter</a></li>
+        <li><a class="icon-gg" title="">Google Plus</a></li>
+      </ul>
+      </div>
+      </div>
+</div>
+
+<div class="adv_box">
+
+<div class="col-md-3">
+<div class="adv_left_box">
+<ul>
+  <li class="icon-1">
+  <h6>Mạng bán vé máy bay và các dịch vụ du lịch Online hiện đại</h6>
+  Ứng dụng công nghệ mới nhất</li>
+    <li class="icon-2">
+  <h6>Luôn có mức giá/dịch vụ tốt nhất</h6>
+  Bảo đảm giá tốt</li>
+    <li class="icon-3">
+  <h6>Dễ dàng và nhanh chóng</h6>
+  Đặt vé chỉ với 3 bước</li>
+    <li class="icon-4">
+  <h6>Hỗ trợ 24/7</h6>
+  Hotline và hỗ trợ trực tuyến
+</li>
+    <li class="icon-5">
+  <h6>Hệ thống thanh toán an toàn</h6>
+  Liên kết với các tổ chức tài chính</li>
+  </ul>
+
+<figure><img src="img/visa.png" alt=""></figure>
+
+</div>
+</div>
+<div class="col-md-6"><img style="border: 3px solid #ddd;" src="img/maps.png" alt="center-box"></div>
+<div class="col-md-3"><img src="img/right-box.png" alt="right-box"></div>
+
+</div>
+
+<div class="footer_box clearfix" style="margin-top:20px">
+<div class="wrap-cnt">
+<div class="col-md-12">
+<div class="seo_box">
+<div class="row">
+<div class="col-md-3">
+<h4><span style="color: #008080;"><strong><span style="font-size: 12pt;"><a style="color: #008080;" href="http://globalink.vn" target="_blank">CTy TNHH ĐTDL Kết Nối Toàn Cầu</a></span></strong></span></h4>
+<ul>
+  <li>[<span style="font-size: 12pt; color: #000000;"><strong>ADD</strong></span>]: 29/38 Đoàn Thị Điểm, Phú Nhuận, TP.HCM, Việt Nam</li>
+  <li>[<span style="font-size: 12pt;"><strong>T</strong></span>]: (+84) 08 399 00 567 - 08 399 00 365</li>
+  <li>[<span style="font-size: 12pt;"><strong>@</strong></span>]: ketnoitoancau@globalink.vn; ketnoitoancau2015@gmail.com</li>
+</ul>
+</div>
+<div class="col-md-3">
+<h4><span style="color: #008000; font-size: 12pt;"><strong><a href="http://globalink.vn" target="_blank">Global Link Travel Investment&nbsp;</a></strong></span></h4>
+<ul class="about-us">
+  <li>V<a href="/gioi-thieu-ve-globalink-vn/">ề Globalink.vn</a></li>
+  <li><a href="/chinh-sach-van-chuyen-hanh-khach/">Chính sách vận chuyển</a></li>
+  <li><a href="/dieu-khoan-thanh-toan/">Điều khoản thanh toán</a></li>
+  <li><a href="/chinh-sach-bao-mat-thong-tin/">Chính sách bảo mật thông tin</a></li>
+  <li><a href="http://globalink.vn/tin-tuc/" target="_blank">Tin Tức</a></li>
+</ul>
+</div>
+<div class="col-md-6 social-network">
+<div class="row">
+<div class="col-md-6">
+<h4><span style="color: #008000;"><strong><a href="http://globalink.vn" target="_blank">Kết nối với chúng tôi:</a></strong></span></h4>
+<ul>
+  <li><a class="icon-fb" title="" href="https://www.facebook.com/Globalink-Travel-1514350022209151/" target="_blank">Facebook</a></li>
+  <li><a class="icon-tt" title="">Twitter</a></li>
+  <li><a class="icon-gg" title="">Google Plus</a></li>
+</ul>
+</div>
+<div class="col-md-6" style="text-align: center;"><a href="http://online.gov.vn/HomePage/CustomWebsiteDisplay.aspx?DocId=19508" target="_blank">
+<img src="img/bo-cong-thuong.png" alt="" class="img-responsive"></a>
+<p style="font-size: 11.5px; color: #15697f; text-align: right;">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;<span style="font-size: 10pt;"><strong>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; <span style="font-family: arial, helvetica, sans-serif;">Officially approved by Ministry of Industry and Trade Vietnam</span></strong></span></p>
+
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="foot_link">
+<div class="foot_inner">Giấy phép ĐKKD số 0313410237 do Sở Kế hoạch và Đầu tư TP.HCM cấp ngày 21/08/2015</div>
+<div class="foot_inner"><a href="http://globalink.vn" target="_blank">Copyright © 2015 <strong>Globalink.vn</strong>. All rights reserved.</a></div>
+</div>
+</div>
+</div>
+</div>
         </div>
     </div>
     <div>
